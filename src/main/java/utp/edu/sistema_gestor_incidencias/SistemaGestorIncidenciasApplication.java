1d0c50992b87f09318983c965a8c417d2925f70a
Jaime Suarez <jaisua8@gmail.com>
2026-04-15 22:00:01 -0500

    Creacion del proyecto sistema de gestion de incidencias

---
+++ src/main/java/utp/edu/sistema_gestor_incidencias/SistemaGestorIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package utp.edu.sistema_gestor_incidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestorIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestorIncidenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/edu/sistema_gestor_incidencias/SistemaGestorIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.edu.sistema_gestor_incidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestorIncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
